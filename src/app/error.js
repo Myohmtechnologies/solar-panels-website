6a515079ce9b2fadbcf4f1948e4d9366edb76c51
Myohmtechnologies <marketing@myohmtechnologies.com>
2024-12-24 12:33:06 +0100

    Fix: Remove duplicate error pages and clean up error handling

--- src/app/error.js
+++
@@ -1,9 +0,0 @@
-export default function Error() {
-  return (
-    <div>
-      <h1>Erreur Serveur</h1>
-      <p>Un problème technique est survenu</p>
-      <a href="/">Retour à l&apos;accueil</a>
-    </div>
-  );
-}
--- src/app/global-error.js
+++
@@ -1,9 +0,0 @@
-export default function GlobalError() {
-  return (
-    <div>
-      <h1>Erreur Système</h1>
-      <p>Un problème technique majeur est survenu</p>
-      <a href="/">Retour à l&apos;accueil</a>
-    </div>
-  );
-}
